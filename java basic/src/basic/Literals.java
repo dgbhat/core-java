52f4afa6027a564d592124e056483df54e2d6f4f
Dattaguru Bhat <dattaguru.bhat@comviva.com>
2021-06-27 13:16:24 +0530

    basics - literals

--- java basic/src/basic/Literals.java
+++ java basic/src/basic/Literals.java
@@ -1,4 +1,8 @@
 package basic;
+/**
+ * Regardless of its complexity, a program always performs operations on numbers, strings, and other values. These values are called literals
+ *
+ * Ex: integer numbers, strings, and characters  */
 
 public class Literals {
     public static void main(String[] args) {
