5cd387ffbf740933506ecda630f7eb59cd10e11e
Dattaguru Bhat <dgbhat99@gmail.com>
2021-07-18 21:57:27 +0530

    basics

--- java basic/src/basic/ArrayBasics.java
+++ java basic/src/basic/ArrayBasics.java
@@ -6,6 +6,9 @@ public class ArrayBasics {
     public static void main(String[] args) {
         printingArray();
         initializingArray();
+        sortingArrays();
+        compareArrays();
+        fillArrays();
     }
 
     static void printingArray() {
@@ -27,10 +30,54 @@ public class ArrayBasics {
     static void initializingArray() {
         /*with default values*/
         int[] numbers; // declaration
-        numbers = new int[10]; // instantiation and initialization with default values
+        numbers = new int[10]; // instantiation and initialization
 
         /*with values*/
-        float[] floatNumbers; // declaration
-        floatNumbers = new float[] { 1.02f, 0.03f, 4f }; // instantiation and initialization
+        float[] floatNumbers;
+        floatNumbers = new float[] { 1.02f, 0.03f, 4f };
     }
+
+    static void sortingArrays() {
+        char[] chars = {'c', 'a', 'd', 'e'};
+        Arrays.sort(chars);
+        System.out.println(chars);
+    }
+
+    static void compareArrays() {
+        int[] numbers1 = { 1, 2, 5, 8 };
+        int[] numbers2 = { 1, 2, 5 };
+        int[] numbers3 = { 1, 2, 5, 8 };
+
+        System.out.println(Arrays.equals(numbers1, numbers2)); //"false"
+        System.out.println(Arrays.equals(numbers1, numbers3)); //"true"
+    }
+
+    static void fillArrays() {
+        int size = 10;
+        char[] characters = new char[size];
+        /*
+        fill(array[], int fromIndex, int toIndex, valueToBeFilled)
+        */
+        Arrays.fill(characters, 0, size / 2, 'A');
+        Arrays.fill(characters, size / 2, size, 'B');
+        System.out.println(Arrays.toString(characters));
+        //[A, A, A, A, A, B, B, B, B, B]
+
+        /*
+        using copyOf
+        copyOf(originalArray[], int newLength)
+        */
+        char[] newCharArray = Arrays.copyOf(characters, 5);
+        char[] newCharArray2 = Arrays.copyOf(characters, 15);
+        System.out.println(Arrays.toString(newCharArray));
+        //[A, A, A, A, A]
+        System.out.println(Arrays.toString(newCharArray2));
+        //[A, A, A, A, A, B, B, B, B, B,  ,  ,  ,  ,  ]
+    }
+
 }
+
+
+/*
+Some methods from "Arrays" class @java.utils
+* */
